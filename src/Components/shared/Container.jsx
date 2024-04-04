f99e726e5505868deff8ec6ac4142b99a80a5022
Ahmdpolash <ahmedpolash732@gmail.com>
2024-04-04 22:28:36 +0600

    contact us page done

--- src/Components/shared/Container.jsx
+++ src/Components/shared/Container.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Container = ({ children }) => {
   return (
     <div>
-      <div className="px-6 md:px-14  lg:px-[90px] py-1 lg:py-3">{children}</div>
+      <div className="px-6 md:px-14  lg:px-[100px] py-1 lg:py-3">{children}</div>
     </div>
   );
 };
